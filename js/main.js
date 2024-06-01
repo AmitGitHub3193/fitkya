4d97741169f737fe2516f3df7f20f60639754fd4
Amit <amitchauhan3193@gmail.com>
2024-06-01 17:11:54 +0530

    pages and css changes

--- js/main.js
+++ js/main.js
@@ -10,66 +10,6 @@ $(document).ready(function () {
         e.preventDefault();
     });
     /** END OF : Hamburger **/
-    
-    /** START OF : Hamburger **/
-    // var swiper = new Swiper('.swiper-workout', {
-    //     effect: 'fade',
-    //     fadeEffect: {
-    //         crossFade: true
-    //     },
-    //     autoplay: {
-    //         delay: 2500,
-    //         disableOnInteraction: false,
-    //     },
-    // });
-
-    // 
-    // var swiper = new Swiper('.swiper-workout', {
-    //     effect: 'fade',
-    //     fadeEffect: {
-    //         crossFade: true
-    //     },
-    //     autoplay: {
-    //         delay: 2500,
-    //         disableOnInteraction: false,
-    //     },
-    //     on: {
-    //         slideChangeTransitionStart: function () {
-    //             // Remove translation classes from all elements
-    //             document.querySelectorAll('.column_img_fir, .column_img_sec, .column_img_thi').forEach(function(element) {
-    //                 element.classList.remove('translated');
-    //             });
-
-    //             // Add translation classes to the elements in the active slide
-    //             var activeSlide = document.querySelector('.swiper-slide-active');
-    //             if (activeSlide) {
-    //                 var imgFir = activeSlide.querySelector('.column_img_fir');
-    //                 var imgSec = activeSlide.querySelector('.column_img_sec');
-    //                 var imgThi = activeSlide.querySelector('.column_img_thi');
-
-    //                 if (imgFir && imgSec && imgThi) {
-    //                     imgFir.classList.add('translated');
-    //                     imgSec.classList.add('translated');
-    //                     imgThi.classList.add('translated');
-    //                 }
-    //             }
-    //         }
-    //     }
-    // });
-    // 
-
-    // var swiper = new Swiper('.swiper-workout', {
-    //     effect: 'fade',
-    //     fadeEffect: {
-    //         crossFade: true
-    //     },
-    //     autoplay: {
-    //         delay: 250000,
-    //         disableOnInteraction: false,
-    //     },
-        
-    // });
-
 
     var swiper = new Swiper('.swiper-workout', {
         effect: 'fade',
@@ -80,15 +20,32 @@ $(document).ready(function () {
             delay: 2500000, // Reduced for testing
             disableOnInteraction: false,
         },
-        // on: {
-        //     slideChange: function() {
-        //         var activeIndex = swiper.activeIndex;
-        //         var rotationDegree = activeIndex * 90;
-        //         document.querySelector('.asasas').style.transition = 'transform 0.5s ease-in-out';
-        //         document.querySelector('.asasas').style.transform = 'rotate(' + rotationDegree + 'deg)';
-        //     }
-        // }
     });
-    
+
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    $('.nav-tabs').on('mousedown touchstart', function (e) {
+        isDown = true;
+        $(this).addClass('nav-tabs-scroll');
+        startX = e.pageX || e.originalEvent.touches[0].pageX;
+        scrollLeft = $(this).scrollLeft();
+        e.preventDefault();
+    });
+
+    $('.nav-tabs').on('mouseleave mouseup touchend', function () {
+        isDown = false;
+        $(this).removeClass('nav-tabs-scroll');
+    });
+
+    $('.nav-tabs').on('mousemove touchmove', function (e) {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX || e.originalEvent.touches[0].pageX;
+        const walk = (x - startX) * 3; // Adjust the scroll speed
+        $(this).scrollLeft(scrollLeft - walk);
+    });
 
 });
